aa4b0be4fff7d59b32bcc6ff00b285fb4d09aeb4
bayedze <156335169+BcK012@users.noreply.github.com>
2025-03-22 19:56:00 +0000

    Add files via upload

--- app.js
+++ app.js
@@ -1,35 +1,42 @@
-//Enpêcher que le formulaire soit envoyer
-let contactForm = document.getElementById("contactForm");
 
-contactForm.addEventListener("submit".function(e)) {
-        e.preventDefault()
+
+function contactForm()                                    
+{ 
+    var prenom = document.forms["fContact"]["prenom"];               
+    var nom = document.forms["fContact"]["nom"];    
+    var sujet = document.forms["fContact"]["sujet"];  
+    var message =  document.forms["fContact"]["message"]; 
+    var contacForm = document.forms["fContact"]["contactForm"]
+ 
+
+    if (prenom.value == "")                                  
+    { 
+        alert("Mettez votre prenom."); 
+        prenom.focus(); 
+        return false; 
+    }    
+    if (nom.value == "")                               
+    { 
+        alert("Mettez votre nom."); 
+        nom.focus(); 
+        return false; 
+    }        
+    if (sujet.value == "")                                   
+    { 
+        alert("Mettez votre sujet."); 
+        sujet.focus(); 
+        return false; 
+    }    
+  
+    
+    if (message.value == "")                  
+    { 
+        alert("Entrez votre message."); 
+        message.focus(); 
+        return false; 
+    } 
+    return true; 
+
+    
+    
 }
-
-//Récupération des données saisies
-const prenom = document.getElementById("prenom").value;
-const nom = document.getElementById("nom").value;
-const sujet = document.getElementById("sujet").value;
-const message = document.getElementById("message").value;
-
-//Validation
-
-if (prenom===" || nom===" || sujet===" || message==="){
-        alert("Veuillez bien remplir tous les champs.");
-        return;
-      
-}
-
-console.log(document.getElementById("prenom :" + prenom));
-console.log(document.getElementById("nom :" + nom));
-console.log(document.getElementById("sujet :" + sujet));
-console.log(document.getElementById("message :" + message));
-
-//Réinitialisation du formulaire.
-
-document.getElementById("contacForm").reset();
-alert("Votre message a été bien envoyé avec succès!");
-
-function redirigerVersProjets() {
-        Window.location.href Id="#projets";
-}
-
